24c917906685e0b4f50ac062a1c76de4178e18ce
pkthunder87 <pkthunder87@gmail.com>
2023-07-27 01:07:53 -0700

    Feat: Add sticky nav functionality for smaller screens

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// Sticky Nav for sidebar
+const sidebar = document.querySelector(".sidebar");
+const galleryNode = document.querySelector(".gallery");
+
+const obsOptions = {
+  root: null,
+  rootMargin: "-3%",
+  threshold: 0.8,
+};
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) sidebar.classList.add("sticky");
+  else sidebar.classList.remove("sticky");
+};
+
+const observerGallery = new IntersectionObserver(stickyNav, obsOptions);
+observerGallery.observe(galleryNode);
